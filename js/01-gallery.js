f53fc10014b7167a520ad42343f10b121692da38
Sergii Tatianchenko <taurus090@gmail.com>
2022-09-03 20:39:10 +0300

    gallery 1

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const GalleryMarkup = createGalleryMarkups(galleryItems);
+// создаем карточу
+function createGalleryMarkups(items) {
+    return items.map(({preview, original, description}) => {
+return`
+<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>
+`;
+}).join(""); 
+}
+
+galleryRef.insertAdjacentHTML('beforeend', GalleryMarkup);
+
+function onClickImgCreateModal(e) {
+	e.preventDefault();
+	if (!e.target.classList.contains("gallery__image")) return;
+	const currentImageUrl = e.target.dataset.source;
+
+	const instance = basicLightbox.create(
+		`
+		<img class="modal__image" src="${currentImageUrl}" />`,
+		{
+			onShow: () => {
+				window.addEventListener("keydown", onKeyPress);
+			},
+			onClose: () => {
+				window.removeEventListener("keydown", onKeyPress);
+			},
+		}
+	);
+
+	instance.show();
+
+	function onKeyPress(event) {
+		const isKeyCode = event.code === "Escape";
+		if (isKeyCode) {
+			instance.close();
+		}
+	}
+
+  galleryRef.addEventListener("click", onClickImgCreateModal);
+
+  console.log(galleryItems) }
