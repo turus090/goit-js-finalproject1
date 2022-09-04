16e8fe835a739146399e99c6fac716e69643766d
Sergii Tatianchenko <taurus090@gmail.com>
2022-09-04 20:30:06 +0300

    add gallery 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryMarkup(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return `
+      <li>
+    <a class="gallery__item" href="${original}">
+	<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+    `;
+		})
+		.join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
 
 console.log(galleryItems);
